f8bb86179b6ce8505be95c99455404df61ad2246
dcmallett <dcmallett28@gmail.com>
2021-08-05 21:43:46 +0100

    Starter files

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h2>{{ title }}</h2>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            requried: true
+        }
+    }
+}
+</script>
